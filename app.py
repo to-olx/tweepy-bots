17038c519810d6e52965531c7733fd5c279b3379
Thulani Ncube <thulani.ncube21@yahoo.com>
2022-12-31 10:15:31 +0000

    Added simple content bot

---
+++ app.py
@@ -0,0 +1,24 @@
+name = ""
+surname = ""
+name = input("What is your name? ")
+surname = input("What is your surname? ")
+
+content = ""  
+content = "I do lifestyle vlogs and Coding, what do you wanna see"
+
+if len(name) > 1 and len(surname) > 1:
+    print(f"Hi {name} {surname}, welcome to my youtube channel. 😍")
+    print(content)
+    print(content)
+    print(content)
+    print(content)
+    print(content)
+    print(content)
+    print(content)
+    print(content)
+
+else:
+    print("Oops, you did not enter your name / surname correctly. 😧")
+    print(name)
+    print(surname)
+    print("Welcome nonetheless! 🙄")
